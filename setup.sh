59925514464a6c221014fc4b255cfff3f7146551
Larissa Krakora <krakorlr@rose-hulman.edu>
2024-02-21 13:09:38 -0500

    setup changes

--- setup.sh
+++ setup.sh
@@ -2,3 +2,11 @@ config-pin P9_11 gpio
 config-pin P9_13 gpio
 config-pin P9_17 gpio
 config-pin P9_19 gpio
+cd ~/sys/class/gpio
+echo 5 > export
+echo 13 > export
+cd gpio5
+echo "out" > direction
+cd ../gpio13
+echo "out" > direction
+# need usb setup
